f55dbe0b8076facc8bc6d129ff53eec4012c8f82
Tri Nguyen <nctri1501@gmail.com>
2021-06-29 15:15:57 +0700

    Finish Level 1

--- src/main/java/io/ubitec/interview_challenges/Range.java
+++ src/main/java/io/ubitec/interview_challenges/Range.java
@@ -2,20 +2,29 @@ package io.ubitec.interview_challenges;
 
 public class Range {
 
+  private final int lowerBound;
+  private final int upperBound;
+
+
   /**
    * Constructor is private BY DESIGN.
    *
    * TODO: Change the constructor to meet your requirements.
    */
-  private Range() {
+  private Range(int lowerBound, int upperBound) {
+    super();
+    if (lowerBound > upperBound) {
+      throw new IllegalArgumentException("lowerBound cannot be greater than upperBound!");
+    }
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
   }
 
   /**
    * Creates a new <b>closed</b> {@code Range} that includes both bounds.
    */
-  public static Range of(int lowerbound, int upperbound) {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+  public static Range of(int lowerBound, int upperBound) {
+    return new Range(lowerBound, upperBound);
   }
 
   /**
@@ -23,24 +32,21 @@ public class Range {
    * {@code Range}.
    */
   public boolean contains(int value) {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return value <= upperBound && value >= lowerBound;
   }
 
   /**
    * Returns the {@code lowerbound} of this {@code Range}.
    */
   public int lowerbound() {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return this.lowerBound;
   }
 
   /**
    * Returns the {@code upperbound} of this {@code Range}.
    */
   public int upperbound() {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return this.upperBound;
   }
 
 }
